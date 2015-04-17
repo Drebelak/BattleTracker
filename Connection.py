0064975037faf16dd1d0bd5b6844be3d73db9d52
Drebelak <drebelak@gmail.com>
2015-04-16 22:21:22 -0500

    Delete Connection.py

--- Connection.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'dylan'
-import MySQLdb as MdB
-
-
-def connect():
-    return MdB.connect(host="engr-cpanel-mysql.engr.illinois.edu",
-                       user="rebelak2_411ad",
-                       passwd="cs411Battle",
-                       db="rebelak2_battle_tracker")
